e77656ff9f8e9890dfa42150947302e05675152c
SHarini99 <harini.souce@gmail.com>
2024-12-30 16:26:38 +0100

    Add files via upload

--- HARINI/sample 2.py
+++ HARINI/sample 2.py
@@ -1,6 +1,74 @@
 def form (a,b):
     c=str(a)+str(b)
     print (c)
-
+ 
 
 form(10,21)
+
+
+x= "Hello World"
+print (x)
+print(type(x))
+
+
+x=20
+print(x)
+print (type(x))
+
+x=20.5
+print(x)
+print(type(x))
+
+
+x=1j
+print(x)
+print(type(x))
+
+x=["apple","banana","cherry"]
+print(x)
+print(type(x))
+
+
+x=("apple","banana","cherry")
+print(x)
+print(type(x))
+
+
+w=range(6)
+print(w)
+print(type(w))
+
+
+x={"name" : "john" , "age" : 36}
+print (type(x))
+
+
+
+x={"apple","banana","cherry"}
+print(type(x))
+
+x=({"apple","banana","cherry"})
+print(type(x))
+
+x= True
+print(x)
+print(type(x))
+
+x=b"Hello"
+print(x)
+print(type(x))
+
+
+x=bytearray(5)
+print(x)
+print(type(x))
+
+
+x=memoryview(bytes(5))
+print(x)
+print(type(x))
+
+
+x= None
+print(x)
+print(type(x))
